df2a35bbf564488418477ba2a2c1175283b28eb3
Kevin R. Barnes <vinbarnes@gmail.com>
2010-08-11 22:08:38 -0500

    Renamed *_work_week methods to *_workweek

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -6,16 +6,16 @@ module DateExtensions
     Time.parse(self.to_s)
   end
 
-  def start_of_work_week
+  def start_of_workweek
     self - (self.cwday - 1)
   end
   
-  def end_of_work_week
-    self.start_of_work_week + 6
+  def end_of_workweek
+    self.start_of_workweek + 6
   end
   
   def this_week
-    start_of_work_week..end_of_work_week
+    start_of_workweek..end_of_workweek
   end
   
 end
