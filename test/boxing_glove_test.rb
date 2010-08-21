766a9a6ffdefa800e1101b9d5906d5c6940e4d0e
Kevin R. Barnes <vinbarnes@gmail.com>
2010-08-21 14:09:30 -0500

    Fix broken test caused by adding more data to test punch file

--- test/boxing_glove_test.rb
+++ test/boxing_glove_test.rb
@@ -89,7 +89,7 @@ class BoxingGloveTest < Test::Unit::TestCase
     it 'returns a list of projects' do
       test_file = File.expand_path(File.dirname(__FILE__) + '/test_punch.yml')
       BoxingGlove.load(test_file)
-      assert_equal ['pet'], BoxingGlove.projects
+      assert_equal ['pet', 'pez'], BoxingGlove.projects.sort
     end
   end
 end
